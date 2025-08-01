271f7db6e31f76573acb538b94588d97d77965c3
Triferp <utsavofficialo@gmail.com>
2025-08-02 01:02:19 +0530

    Fix: Corrected case sensitivity in Header import

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 
 // Component Imports
-import Header from './components/header';
-import Footer from './components/footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 // Page Imports
 import HomePage from './pages/HomePage';
